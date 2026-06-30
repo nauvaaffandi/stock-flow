91e5bb04dc47128c1fb64fb75afb28425e25fc71
naufandi <fadilahaffandi696@gmail.com>
2026-06-30 21:24:27 +0700

    feat: init stock movement service

---
+++ backend/src/modules/inventory/application/services/stock-movement.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import { StockMovementAbstract } from '../../domain/interfaces/stock-movement.abstract'
+
+@Injectable()
+export class StockMovementService implements StockMovementAbstract {
+    
+}
--- backend/src/modules/inventory/inventory.module.ts
+++ backend/src/modules/inventory/inventory.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common'
+import { DrizzleModule } from '../../infrastructure/drizzle/drizzle.module'
 
-@Module({})
+import { StockMovementAbstract } from './domain/interfaces/stock-movement.abstract'
+import { StockMovementService } from './application/services/stock-movement.service'
+
+@Module({
+    imports: [DrizzleModule],
+    providers: [
+        {
+            provide: StockMovementAbstract,
+            useClass: StockMovementService,
+        },
+        
+        StockMovementService,
+    ],
+})
 export class InventoryModule {}
+    
