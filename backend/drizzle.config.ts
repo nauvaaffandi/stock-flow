1cb651d6179b5c4ea5bd722141cbeafe6fc73608
naufandi <fadilahaffandi696@gmail.com>
2026-06-30 14:57:51 +0700

    fix drizzle config.out to ./.drizzle

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 console.log(process.env.DATABASE_URL)
 
 export default defineConfig({
-    out: './drizzle', 
+    out: './.drizzle', 
     schema: [
         './src/modules/*/infrastructure/drizzle/schema.ts',
         './src/infrastructure/drizzle/schema.ts'
