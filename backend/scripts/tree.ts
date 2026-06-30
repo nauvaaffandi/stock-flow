68b41092a9168cfe884d78f962450552b10c96ad
naufandi <fadilahaffandi696@gmail.com>
2026-06-30 18:30:11 +0700

    chore modified scripts/tree.ts write to internal/tree.json

--- backend/scripts/tree.ts
+++ backend/scripts/tree.ts
@@ -1,7 +1,22 @@
-import dirTree from 'directory-tree'
+import dirTree from 'directory-tree';
+import * as fs from 'fs';
+import * as path from 'path';
+
 
 const tree = dirTree(process.cwd(), {
   exclude: /node_modules|\.git|dist|coverage|\.next/
 })
 
-console.log(JSON.stringify(tree))
+const jsonString = JSON.stringify(tree, null, 4); // null, 2 biar rapi (indented)
+
+console.log(tree);
+
+const filePath = path.join(process.cwd(), 'internal', 'tree.json');
+
+
+const dir = path.dirname(filePath);
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+
+fs.writeFileSync(filePath, jsonString, 'utf-8');
