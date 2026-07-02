5a39ceb8756013a1f747a26a402231fcc146c819
naufandi <fadilahaffandi696@gmail.com>
2026-07-02 22:49:10 +0700

    fix(global filter): implement new logger)

--- backend/src/shared/filters/global-error.filter.ts
+++ backend/src/shared/filters/global-error.filter.ts
@@ -6,23 +6,45 @@ import {
 } from '@nestjs/common'
 import { ErrorTelemetryService } from '../../infrastructure/logging'
 import { Response, Request } from 'express'
+import { LoggerService } from '../../infrastructure/logging/logger.service'
+
 
 @Catch()
 export class GlobalErrorFilter implements ExceptionFilter {
-	constructor(private readonly errorTelemetry: ErrorTelemetryService) {}
+	constructor(
+        private readonly logger: LoggerService
+    ) {}
 
 	catch(err: any, host: ArgumentsHost) {
 		const ctx = host.switchToHttp()
 		const res = ctx.getResponse<Response>()
 		const req = ctx.getRequest<Request>()
-
-		this.errorTelemetry.logger.error('Server Error occured', {
-			method: req.method,
-			reqId: req.headers['x-request-id'] || 'reqId-undefined',
-			path: req.url,
-			stack: err.stack,
-		})
-
+        
+        this.logger.error({
+            message: 'Server Error Occured',
+            method: req.method,
+            requestId: req.headers['x-request-id'] || 'reqId-xx',
+            path: err.url,
+            context: 'SERVER_ERROR',
+            statusCode: 500,
+            stack: err.stack,
+            metadata: {
+                ...(err.code ? { code: err.code } : {}),
+                ...(err.name ? { name: err.name } : {}),
+                ...(err.errno ? { errno: err.errno } : {}),
+                ...(err.detail ? { detail: err.detail } : {}),
+                ...(err.hint ? { hint: err.hint } : {}),
+                ...(err.schema ? { schema: err.schema } : {}),
+                ...(err.table ? { table: err.table } : {}),
+                ...(err.column ? { column: err.column } : {}),
+                ...(err.constraint ? { constraint: err.constraint } : {}),
+                ...(err.routine ? { routine: err.routine } : {}),
+                ...(err.cause ? { cause: err.cause } : {}),
+            },
+            service: 'global-error-filter',
+            duration: Date.now() - req.system.startTime
+        })
+        
 		return res.status(500).json({
 			success: false,
 			errors: {
