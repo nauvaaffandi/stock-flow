20c2493c160b88fe0b902f6240fc3b663b6bed14
naufandi <fadilahaffandi696@gmail.com>
2026-07-14 15:14:19 +0000

    edit zod schema

--- backend/src/modules/sales/features/transaction-items/presentation/validation/create-transaction-item.zod.validation.ts
+++ backend/src/modules/sales/features/transaction-items/presentation/validation/create-transaction-item.zod.validation.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod'
 
-export const CreateTransactionItemZodValidation = z
+export const CreateTransactionItemZodValidation = z.object({
+    productId: z.string().min(3),
+    unitId: z.string().min(3),
+    quantity: z.number(),
+    unitPrice: z.number(),       
+    unitCost: z.number(),
+    subtotal: z.number(),
+})
 
 
 
